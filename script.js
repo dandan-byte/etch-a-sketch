1b09b7af9eb6cf9c1088a3b3b7cf388adbeff583
dandan-byte <75590778+dandan-byte@users.noreply.github.com>
2025-07-06 22:23:00 +0000

    Change createGrid logic to more aptly class resulting divs.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-//creates grid a square grid with a side of squares length.
+//  creates a square grid with a side of squares length.
 const createGrid = function(squares){
     const container = document.getElementById("grid-container");
     for(let i = 0; i < squares * squares; i++){
         const div = document.createElement("div")
-        div.setAttribute('id', 'div-#${i}');
+        div.setAttribute('class', 'grid-square');
         container.appendChild(div);
     };
 };
